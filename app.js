9b575ee0d93743bce8e365e1c7a7b628b4dec759
yihwan <yihwankim@gmail.com>
2018-05-25 15:36:40 -0700

    implement hacky delete functionality

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const checkboxes = document.querySelectorAll('.list input[type="checkbox"]');
+let checkboxes = document.querySelectorAll('.list input[type="checkbox"]');
+let deleteIcons = document.querySelectorAll('.list .delete');
 
 let lastChecked;
 
 window.onload = function() {
   document.getElementById('todo-count').innerHTML = countTodos();
+  deleteIcons.forEach(icon => {
+    icon.style.visibility = "hidden";
+  });
 };
 
 function countTodos() {
@@ -34,14 +38,57 @@ function createTodo() {
   let newHtml =
   `
   <div class="item">
+    <span onclick="deleteTodo(this);"
+    class="delete">❌</span>
     <input type="checkbox">
     <p>${enteredTodo.value}</p>
   </div>
   `;
-  const todoItems = document.querySelector('.items');
 
+  const todoItems = document.querySelector('.items');
   todoItems.insertAdjacentHTML('beforeend', newHtml);
+
+  checkboxes = document.querySelectorAll('.list input[type="checkbox"]');
+  deleteIcons = document.querySelectorAll('.list .delete');
+
+  deleteIcons[deleteIcons.length - 1].style.visibility = "hidden";
   document.getElementById('todo-count').innerHTML = countTodos();
+}
+
+function toggleEdit() {
+  const editButton = document.getElementById('edit');
+
+  if (editButton.innerHTML === "Edit") {
+    editButton.innerHTML = "Done";
+
+    document.querySelector('.new').style.visibility = "hidden";
+
+    checkboxes.forEach(checkbox => {
+      checkbox.style.visibility = "hidden";
+    });
+
+    deleteIcons.forEach(icon => {
+      icon.style.visibility = "visible";
+    });
+
+  } else if (editButton.innerHTML === "Done") {
+    editButton.innerHTML = "Edit";
+
+    document.querySelector('.new').style.visibility = "visible";
+
+    checkboxes.forEach(checkbox => {
+      checkbox.style.visibility = "visible";
+    });
+
+    deleteIcons.forEach(icon => {
+      icon.style.visibility = "hidden";
+    });
+  }
+}
+
+function deleteTodo(todo) {
+  console.log(todo);
+  todo.parentNode.parentNode.removeChild(todo.parentNode);
 }
 
 checkboxes.forEach(checkbox => {
